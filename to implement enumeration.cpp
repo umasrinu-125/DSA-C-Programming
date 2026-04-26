c533c232311d17841b5230786f855cf0a847d3d5
UMA SRINU <umasrinupampana125890@gmail.com>
2026-04-26 17:49:46 +0530

    Add files via upload

---
+++ to implement enumeration.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()  
+{
+    enum week{mon, tur, wed, thu, fri, sat, sun};  
+    printf("wed=%d\n", wed);    
+    printf("fri=%d\n", fri);
+    printf("sun=%d\n", sun);
+  return 0;
+}
